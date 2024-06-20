2024d646507ed3d706caf3f694a13362981f0c72
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-20 13:37:35 +0900

    Update phpcomplete formula with multiple PHP versions and PECL packages
    This commit updates the phpcomplete formula to support multiple versions of PHP and includes the installation of XDebug and other PECL packages. These changes also include necessary dependencies, like imagemagick and libmemcached. Lastly, the formula is updated with tests to ensure the functionality of each PHP version and PECL extension.

--- phpcomplete.rb
+++ phpcomplete.rb
@@ -1,33 +1,108 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Phpcomplete < Formula
-  desc ""
-  homepage ""
-  url "Phpcomplete"
+  desc "Install multiple versions of PHP with XDebug and other PECL packages"
+  homepage "https://www.php.net/"
   version "0.1.0"
+  url "file:///dev/null"
   sha256 ""
-  license ""
+  license "MIT"
 
-  # depends_on "cmake" => :build
+  depends_on "shivammathur/php/php@5.6"
+  depends_on "shivammathur/php/php@7.0"
+  depends_on "shivammathur/php/php@7.1"
+  depends_on "shivammathur/php/php@7.2"
+  depends_on "shivammathur/php/php@7.3"
+  depends_on "shivammathur/php/php@7.4"
+  depends_on "shivammathur/php/php@8.0"
+  depends_on "shivammathur/php/php@8.1"
+  depends_on "shivammathur/php/php@8.2"
+  depends_on "shivammathur/php/php@8.3"
+  depends_on "imagemagick"
+  depends_on "libmemcached"
+  depends_on "pkg-config"
+  depends_on "zlib"
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    versions = {
+      "8.1" => "",
+      "8.2" => "",
+      "7.4" => "3.1.6",
+      "7.3" => "3.1.6",
+      "7.2" => "3.1.6",
+      "7.1" => "2.9.8",
+      "7.0" => "2.7.2",
+      "5.6" => "2.5.5"
+    }
+
+    versions.each do |version, xdebug_version|
+      php_prefix = "#{HOMEBREW_PREFIX}/opt/php@#{version}"
+      ext_dir = `#{php_prefix}/bin/php-config --extension-dir`.strip
+
+      # Check if xdebug is installed
+      if File.exist?("#{ext_dir}/xdebug.so")
+        system "#{php_prefix}/bin/pecl upgrade xdebug || true"
+      else
+        if xdebug_version.empty?
+          system "#{php_prefix}/bin/pecl install xdebug || true"
+        else
+          system "#{php_prefix}/bin/pecl install xdebug-#{xdebug_version} || true"
+        end
+      end
+
+      # Install other PECL packages
+      if version == "8.2"
+        if File.exist?("#{ext_dir}/pcov.so")
+          system "#{php_prefix}/bin/pecl upgrade pcov || true"
+        else
+          system "#{php_prefix}/bin/pecl install pcov || true"
+        end
+
+        if File.exist?("#{ext_dir}/apcu.so")
+          system "#{php_prefix}/bin/pecl upgrade apcu || true"
+        else
+          system "#{php_prefix}/bin/pecl install apcu || true"
+        end
+
+        if File.exist?("#{ext_dir}/redis.so")
+          system "#{php_prefix}/bin/pecl upgrade redis || true"
+        else
+          system "#{php_prefix}/bin/pecl install redis || true"
+        end
+
+        if File.exist?("#{ext_dir}/xhprof.so")
+          system "#{php_prefix}/bin/pecl upgrade xhprof || true"
+        else
+          system "#{php_prefix}/bin/pecl install xhprof || true"
+        end
+      end
+
+      if File.exist?("#{ext_dir}/memcached.so")
+        system "yes no | PHP_ZLIB_DIR=$(brew --prefix zlib) #{php_prefix}/bin/pecl upgrade memcached || true"
+      else
+        system "yes no | PHP_ZLIB_DIR=$(brew --prefix zlib) #{php_prefix}/bin/pecl install memcached || true"
+      end
+
+      if File.exist?("#{ext_dir}/imagick.so")
+        system "yes no | #{php_prefix}/bin/pecl upgrade imagick || true"
+      else
+        system "yes no | #{php_prefix}/bin/pecl install imagick || true"
+      end
+    end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Phpcomplete`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    versions = ["5.6", "7.0", "7.1", "7.2", "7.3", "7.4", "8.0", "8.1", "8.2", "8.3"]
+    versions.each do |version|
+      php_prefix = "#{HOMEBREW_PREFIX}/opt/php@#{version}"
+      system "#{php_prefix}/bin/php -v"
+
+      # Test each PECL extension
+      system "#{php_prefix}/bin/php -m | grep xdebug || true"
+      system "#{php_prefix}/bin/php -m | grep pcov || true" if version.to_f >= 8.2
+      system "#{php_prefix}/bin/php -m | grep apcu || true" if version.to_f >= 8.2
+      system "#{php_prefix}/bin/php -m | grep redis || true" if version.to_f >= 8.2
+      system "#{php_prefix}/bin/php -m | grep xhprof || true" if version.to_f >= 8.2
+      system "#{php_prefix}/bin/php -m | grep memcached || true"
+      system "#{php_prefix}/bin/php -m | grep imagick || true"
+    end
   end
 end
