112e8ac7d72a556b55ad026a833c897e07432a5c
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-20 13:03:57 +0900

    Add Phpcomplete formula
    Created a new `phpcomplete` formula in Homebrew. The formula includes basic information about `phpcomplete`, scaffolding for its installation, and a placeholder test command for its functionality. An improvement to the test command will be needed before final submission.

---
+++ phpcomplete.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Phpcomplete < Formula
+  desc ""
+  homepage ""
+  url "Phpcomplete"
+  version "0.1.0"
+  sha256 ""
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Phpcomplete`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
