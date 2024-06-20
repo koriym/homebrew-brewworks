dbd2989e4adcdb998c731a781351af23bf7ac338
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-20 16:42:21 +0900

    Refine phpcomplete.rb and eliminate redundancy
    To avoid code duplication and increase readability, the code has been restructured. PECL installation versions, PHP versions tied with specific Xdebug versions, and extensions-to-be-installed data have now been defined as constants. Additionally, an explicit check has been added to verify whether an extension is already installed before initiating a new installation.

--- phpcomplete.rb
+++ phpcomplete.rb
@@ -22,50 +22,47 @@ class Phpcomplete < Formula
   depends_on "pkg-config"
   depends_on "zlib"
 
+  # PHP versions with additional PECL packages
+  PECL_INSTALL_VERSIONS = ["8.1", "8.2", "8.3"].freeze
+
+  # PHP version => Xdebug version
+  # Please comment out any unnecessary PHP versions.
+  VERSIONS = {
+    "8.3" => "",
+    "8.2" => "",
+    "8.1" => "",
+    "8.0" => "",
+    "7.4" => "3.1.6",
+    "7.3" => "3.1.6",
+    "7.2" => "3.1.6",
+    "7.1" => "2.9.8",
+    "7.0" => "2.7.2",
+    "5.6" => "2.5.5"
+  }.freeze
+
+  # Extensions to be installed along with optional special commands
+  # 'memcached' requires the path to zlib, hence the special command
+   EXTENSIONS = {
+    'pcov' => "",                # Code coverage driver: https://pecl.php.net/package/pcov
+    'apcu' => "",                # APC User Cache: https://pecl.php.net/package/apcu
+    'redis' => "",               # Redis client: https://pecl.php.net/package/redis
+    'xhprof' => "",              # Hierarchical Profiler: https://pecl.php.net/package/xhprof
+    'imagick' => "",             # Image processing library: https://pecl.php.net/package/imagick
+    # Memcached client: https://pecl.php.net/package/memcached
+    'memcached' => "PHP_ZLIB_DIR=$(brew --prefix zlib)"
+  }.freeze
+
   def install
-    # PHP versions with additional PECL packages
-    pecl_install_versions = ["8.1", "8.2", "8.3"]
-
-    versions = {
-      "8.3" => "",
-      "8.2" => "",
-      "8.1" => "",
-      "8.0" => "",
-      "7.4" => "3.1.6",
-      "7.3" => "3.1.6",
-      "7.2" => "3.1.6",
-      "7.1" => "2.9.8",
-      "7.0" => "2.7.2",
-      "5.6" => "2.5.5"
-    }
-
-    # Extensions to be installed along with optional special commands
-    # 'memcached' requires the path to zlib, hence the special command
-    extensions = {
-      'pcov' => "",                # Code coverage driver: https://pecl.php.net/package/pcov
-      'apcu' => "",                # APC User Cache: https://pecl.php.net/package/apcu
-      'redis' => "",               # Redis client: https://pecl.php.net/package/redis
-      'xhprof' => "",              # Hierarchical Profiler: https://pecl.php.net/package/xhprof
-      'imagick' => "",             # Image processing library: https://pecl.php.net/package/imagick
-                                   # Memcached client: https://pecl.php.net/package/memcached
-      'memcached' => "PHP_ZLIB_DIR=$(brew --prefix zlib)"
-    }
-
-    versions.each do |version, xdebug_version|
+    VERSIONS.each do |version, xdebug_version|
       php_prefix = "#{HOMEBREW_PREFIX}/opt/php@#{version}"
       ext_dir = `#{php_prefix}/bin/php-config --extension-dir`.strip
 
       # Install xdebug for all versions
-      if File.exist?("#{ext_dir}/xdebug.so")
-        system "#{php_prefix}/bin/pecl upgrade xdebug || true"
-      else
-        xdebug = xdebug_version.empty? ? "xdebug" : "xdebug-#{xdebug_version}"
-        system "#{php_prefix}/bin/pecl install #{xdebug} || true"
-      end
+      handle_pecl_installation(php_prefix, ext_dir, "xdebug", xdebug_version.empty? ? "" : xdebug_version)
 
       # Install other PECL packages only for supported versions
-      if pecl_install_versions.include?(version)
-        extensions.each do |extension, cmd|
+      if PECL_INSTALL_VERSIONS.include?(version)
+        EXTENSIONS.each do |extension, cmd|
           handle_pecl_installation(php_prefix, ext_dir, extension, cmd)
         end
       end
@@ -76,8 +73,11 @@ class Phpcomplete < Formula
   end
 
   def handle_pecl_installation(php_prefix, ext_dir, extension_name, special_cmd = "")
-    command = File.exist?("#{ext_dir}/#{extension_name}.so") ? "upgrade" : "install"
-    system "yes no | #{special_cmd} #{php_prefix}/bin/pecl #{command} #{extension_name} || true"
+    if File.exist?("#{ext_dir}/#{extension_name}.so")
+      puts "#{extension_name} is already installed for PHP #{php_prefix.split('@').last}"
+    else
+      system "yes no | #{special_cmd} #{php_prefix}/bin/pecl install #{extension_name} || true"
+    end
   end
 
   test do
