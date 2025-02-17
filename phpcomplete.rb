222fe80107ca9515043803a7b3e56fa2cf09d694
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-02-18 01:29:03 +0900

    Add support for PHP 8.4 dependencies and configuration.
    Updated the script to include PHP 8.4 in the dependencies and PECL installation versions. Extended the version mapping to account for PHP 8.4, ensuring compatibility with the latest PHP release.

--- phpcomplete.rb
+++ phpcomplete.rb
@@ -17,17 +17,19 @@ class Phpcomplete < Formula
   depends_on "shivammathur/php/php@8.1"
   depends_on "shivammathur/php/php@8.2"
   depends_on "shivammathur/php/php@8.3"
+  depends_on "shivammathur/php/php@8.4"
   depends_on "imagemagick"
   depends_on "libmemcached"
   depends_on "pkg-config"
   depends_on "zlib"
 
   # PHP versions with additional PECL packages
-  PECL_INSTALL_VERSIONS = ["8.1", "8.2", "8.3"].freeze
+  PECL_INSTALL_VERSIONS = ["8.1", "8.2", "8.3", "8.4"].freeze
 
   # PHP version => Xdebug version
   # Please comment out any unnecessary PHP versions.
   VERSIONS = {
+    "8.4" => "",
     "8.3" => "",
     "8.2" => "",
     "8.1" => "",
